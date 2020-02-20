293070e5225d3c7b5299309775b4f6005a2802bd
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-20 23:35:28 +0200

    feat: implemented reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+var result = '';
+var array = n.toString().split('');
+  for (var i = array.length - 1; i >= 0; i--) {
+        result = result + array[i];
+  }
+  console.log(result);
+  return parseInt(result);
 }
